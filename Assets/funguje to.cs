36bd9a859de6ec5d19da92ea0b429f52ddf62086
Pechy6 <Pechyy6@gmail.com>
2023-11-11 13:21:26 +0100

    Update funguje to.cs

--- Assets/funguje to.cs
+++ Assets/funguje to.cs
@@ -7,7 +7,7 @@ public class fungujeto : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("A ted se to prepise znova bez problemu ? ");
     }
 
     // Update is called once per frame
