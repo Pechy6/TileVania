eddcef95730acff5be776dc9a439a75fa5e0f4c0
Pechy6 <Pechyy6@gmail.com>
2023-11-16 17:23:02 +0100

    Enemy

---
+++ Assets/Scripts/Enemy Movement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 1f;
+    Rigidbody2D myRigidbody;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2(movementSpeed, 0f);
+    }
+}
